e40d857316eea6677880d552a386e83576ef92c5
antolabarca <anto.labarca.s@gmail.com>
2020-04-20 16:46:04 -0400

    entrega parcial 1

--- src/main/java/com/github/cc3002/citricjuice/model/board/Panel.java
+++ src/main/java/com/github/cc3002/citricjuice/model/board/Panel.java
@@ -14,48 +14,14 @@ import java.util.Set;
  * @version 1.0.6-rc.2
  * @since 1.0
  */
-public class Panel {
-  private final PanelType type;
+public class Panel { /*this is a neutral panel*/
+  //private final PanelType type;
   private final Set<Panel> nextPanels = new HashSet<>();
 
   /**
    * Creates a new panel.
-   *
-   * @param type
-   *     the type of the panel.
    */
-  public Panel(final PanelType type) {
-    this.type = type;
-  }
-
-  /**
-   * Restores a player's HP in 1.
-   */
-  private static void applyHealTo(final @NotNull Player player) {
-    player.setCurrentHP(player.getCurrentHP() + 1);
-  }
-
-  /**
-   * Reduces the player's star count by the D6 roll multiplied by the player's norma level.
-   */
-  private static void applyDropTo(final @NotNull Player player) {
-    player.reduceStarsBy(player.roll() * player.getNormaLevel());
-  }
-
-  /**
-   * Reduces the player's star count by the D6 roll multiplied by the maximum between the player's
-   * norma level and three.
-   */
-  private static void applyBonusTo(final @NotNull Player player) {
-    player.increaseStarsBy(player.roll() * Math.min(player.getNormaLevel(), 3));
-  }
-
-  /**
-   * Returns the type of this panel
-   */
-  public PanelType getType() {
-    return type;
-  }
+  public Panel() { }
 
   /**
    * Returns a copy of this panel's next ones.
@@ -75,21 +41,71 @@ public class Panel {
   }
 
   /**
-   * Executes the appropriate action to the player according to this panel's type.
+   * Creo que no es necesario dejar este metodo, ya que en este caso no hace nada, pero originalmente
+   * también estaba el caso de que type=neutral en activatedBy, asi que preferi dejarlo por si acaso
    */
   public void activatedBy(final Player player) {
-    switch (type) {
-      case BONUS:
-        applyBonusTo(player);
-        break;
-      case DROP:
-        applyDropTo(player);
-        break;
-      case HOME:
-        applyHealTo(player);
-        break;
-      case NEUTRAL:
-        break;
-    }
+    return null;
   }
 }
+
+
+public class BonusPanel extends Panel {
+
+  public BonusPanel(){
+    super();
+  }
+
+  /**
+   * Increases the player's star count by the D6 roll multiplied by the maximum between the player's
+   * norma level and three.
+   */
+  private static void activatedBy(final @NotNull Player player) {
+    player.increaseStarsBy(player.roll() * Math.min(player.getNormaLevel(), 3));
+  }
+}
+
+
+public class DropPanel extends Panel{
+
+  public DropPanel(){
+    super();
+  }
+
+  /**
+   * Reduces the player's star count by the D6 roll multiplied by the player's norma level.
+   */
+  private static void activatedBy(final @NotNull Player player) {
+    player.reduceStarsBy(player.roll() * player.getNormaLevel());
+  }
+}
+
+
+public class HomePanel extends Panel{
+
+  public HomePanel(){
+    super();
+  }
+
+  /**
+   * Restores a player's HP in 1.
+   */
+  private static void activatedBy(final @NotNull Player player) {
+    player.setCurrentHP(player.getCurrentHP() + 1);
+  }
+}
+
+public class EncounterPanel extends Panel{
+
+  public EncounterPanel(){
+    super();
+  }
+}
+
+public class BossPanel extends Panel{
+
+  public BossPanel(){
+    super();
+  }
+}
+
